bb0fa1a43674a101bee4d4b9d9ba2b6c44ef5202
yuxra <rasikahrasikah3@gmail.com>
2025-03-20 21:21:47 +0700

    menambah login

---
+++ src/views/login.php
@@ -0,0 +1,5 @@
+<form action="../controllers/login_process.php" method="POST">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
