b6a55602650a658314834edd2c77dcc9bb13829a
yuxra <rasikahrasikah3@gmail.com>
2025-03-20 21:15:42 +0700

    Menambahkan koneksi ke db

---
+++ src/config/database.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "pendaftaran_pkl";
+$user = "postgres";
+$password = "rsu333007"; 
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
