28914464b7e029f356d2212c03ea0b5cc8bcbb23
yuxra <rasikahrasikah3@gmail.com>
2025-03-27 06:04:13 +0700

    update system register

--- src/controllers/register_process.php
+++ src/controllers/register_process.php
@@ -1,15 +1,24 @@
 <?php
-include '../config/database.php';
+include '../../config/database.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $email = $_POST["email"];
     $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
 
-    $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute(["username" => $username, "email" => $email, "password" => $password]);
+    if (empty($username) || empty($email) || empty($password)) {
+        die("pastikan kamu sudah mengisi semua kolom dengan benar");
+    }
 
-    echo "Pendaftaran berhasil!";
+    $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+    $result = pg_query($conn, $query)
+
+    if ($result) {
+        echo "register berhasil! silahkan <a href='../../login.php'>Login</a>";
+    } else {
+        echo "gagal registrasi: " . pg_last_error($conn);
+    }
+} else {
+    echo "akses ditolak";
 }
 ?>
