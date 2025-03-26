16c45e22e6a2ebc4ae27f1bd8eec3535f8311fe8
yuxra <rasikahrasikah3@gmail.com>
2025-03-27 06:16:03 +0700

    update system login

--- src/controllers/login_process.php
+++ src/controllers/login_process.php
@@ -1,21 +1,33 @@
 <?php
-include '../config/database.php';
+include '../../config/database.php';
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    $query = "SELECT * FROM users WHERE email = :email";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute(["email" => $email]);
-    $user = $stmt->fetch();
-
-    if ($user && password_verify($password, $user["password"])) {
-        $_SESSION["user_id"] = $user["id"];
-        header("Location: dashboard.php");
-    } else {
-        echo "Login gagal, coba lagi";
+    if (empty($email) || empty($password)) {
+        die("email dan password harus diisi!");
     }
+
+    $query = "SELECT * FROM users WHERE email = :'$email'";
+    $result = pg_query($conn, $query);
+    $user = pg_fetch_assoc($result);
+
+    if ($user) {
+            if (password_verify($password, $user["password"])) {
+            $_SESSION["user"] = $user;
+            header("Location: ../../dashboard.php");
+            
+        } else {
+            echo "password salah";
+        }
+
+    } else {
+        echo "email tidak ditemukan";
+    }
+    
+} else {
+    echo "akses ditolak";
 }
 ?>
