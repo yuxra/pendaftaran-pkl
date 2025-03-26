21f587d8197f541eab6d18d1639768884e22b732
yuxra <rasikahrasikah3@gmail.com>
2025-03-27 06:42:18 +0700

    menambahkan system profile

---
+++ src/controllers/profile_process.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include("../../config/database.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST["id"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+
+    if (empty($id) || empty($username) || empty($email)) {
+        die("pastikan kamu sudah mengisi semua kolom dengan benar");
+    }
+
+    $query = "UPDATE users SET username = '$username', email = '$email' WHERE id = $id";
+    $result = pg_query($conn, $query)
+
+    if ($result) {
+        $_SESSION["user"]["username"] = $username;
+        $_SESSION["user"]["email"] = $email;
+        header("location: ../../profile.php");
+        exit;
+    } else {
+        echo "gagal update profile: " . pg_last_error($conn);
+    }
+} else {
+    echo "akses ditolak!";
+}
+
+?>
