a358729784cd8060a7f05661306dee12c917fb57
yuxra <rasikahrasikah3@gmail.com>
2025-03-27 05:52:46 +0700

    update database

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="src/controllers/login_process.php" method="POST">
+    Email: <input type="email" name="email" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit">Login</button>
+</form>
--- src/config/database.php
+++ src/config/database.php
@@ -4,10 +4,9 @@ $dbname = "pendaftaran_pkl";
 $user = "postgres";
 $password = "rsu333007"; 
 
-try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
+$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+
+if (!$conn) {
+    die("koneksi database gagal: " . pg_last_error());
 }
 ?>
