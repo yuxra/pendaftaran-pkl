8e71fe418b9edf0c97a3cc705b5d153ae0f49f7f
yuxra <rasikahrasikah3@gmail.com>
2025-03-28 11:13:20 +0700

    menambahkan html register

--- src/views/register.php
+++ src/views/register.php
@@ -1,6 +1,14 @@
-<form action="../controllers/register_process.php" method="POST">
-    <input type="text" name="username" placeholder="Username" required>
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Daftar</button>
-</form>
+<link rel="stylesheet" href="../public/styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Registrasi PKL</h1>
+        <form action="../controllers/register_process.php" method="POST">
+            <input type="text" name="name" placeholder="Nama Lengkap" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Daftar</button>
+        </form>
+        <p>Sudah punya akun? <a href="login.php">Masuk di sini</a></p>
+    </div>
+</body>
