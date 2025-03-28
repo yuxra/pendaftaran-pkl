db6fc084e4512c3477cbad3f577187fcc0730f19
yuxra <rasikahrasikah3@gmail.com>
2025-03-28 11:11:09 +0700

    menambahkan html login

--- src/views/login.php
+++ src/views/login.php
@@ -6,14 +6,16 @@
     <title>login</title>
 </head>
 <body>
-    <h1>Selamat datang kembali!</h1>
-    <h2>silahkan login terlebih dahulu</h2>
-
-    <form action="../controllers/login_process.php" method="POST">
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-    </form>    
+    <div class="container">
+        <h1>Selamat Datang Kembali</h1>
+        <p>Silakan masuk terlebih dahulu untuk melanjutkan.</p>
+        <form action="../controllers/login_process.php" method="POST">
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Masuk</button>
+        </form>
+        <p>Belum punya akun? <a href="register.php">Daftar sekarang</a></p>
+    </div>
 </body>
 </html>
 
