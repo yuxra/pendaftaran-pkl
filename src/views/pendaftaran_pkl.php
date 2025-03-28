8f0d8c1d5611afde067a2a6264e614fac3c59508
yuxra <rasikahrasikah3@gmail.com>
2025-03-28 12:15:19 +0700

    menambahkan css pendaftaran sederhana

--- src/views/pendaftaran_pkl.php
+++ src/views/pendaftaran_pkl.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Pendaftaran PKL</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../public/pendaftaran.css">
 </head>
 <body>
 
