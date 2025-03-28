698d619bdbc516bdec31b0d24fdf03ac7501004f
yuxra <rasikahrasikah3@gmail.com>
2025-03-28 10:31:29 +0700

    menambahkan style css

--- src/views/login.php
+++ src/views/login.php
@@ -1,5 +1,20 @@
-<form action="../controllers/login_process.php" method="POST">
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Looogin</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <h1>Selamat datang kembali!</h1>
+    <h2>silahkan login terlebih dahulu</h2>
+
+    <form action="../controllers/login_process.php" method="POST">
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>    
+</body>
+</html>
+
+
